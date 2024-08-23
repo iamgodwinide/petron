6df64a6c04588e9282aaeeb1b69763b6273b95f1
Godwin Idemudia <godwinide@gmail.com>
2024-08-23 17:56:55 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+
+const styleBoxes1 = () => {
+
+    Array(3).fill(0).forEach((_, i) => {
+        const box = document.getElementById(`box${i + 1}`);
+
+        const area = document.getElementById(`area${i + 1}`);
+
+        const coords = area.coords.split(",");
+
+        box.style.width = (Math.abs(coords[0] - coords[2])) + "px"
+        box.style.height = (Math.abs(coords[1] - coords[3])) + "px"
+        box.style.left = Number(coords[0]) + "px"
+        box.style.top = Number(coords[1]) + "px"
+    });
+}
+
+const styleBoxes2 = () => {
+
+    Array(4).fill(0).forEach((_, i) => {
+        const box = document.getElementById(`box${i + 3}`);
+
+        const area = document.getElementById(`area${i + 3}`);
+
+        const coords = area.coords.split(",");
+
+        box.style.width = (Math.abs(coords[0] - coords[2])) + "px"
+        box.style.height = (Math.abs(coords[1] - coords[3])) + "px"
+        box.style.left = Number(coords[0]) + "px"
+        box.style.top = Number(coords[1]) + "px"
+    });
+}
+
+setTimeout(() => {
+    if (window.innerWidth >= 360) {
+        styleBoxes1()
+    } else {
+        styleBoxes2()
+    }
+}, 1200)
+
+
+
